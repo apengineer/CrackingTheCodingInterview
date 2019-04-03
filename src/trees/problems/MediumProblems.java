79e157d095f0c717efe31bb13eb3ca3090720cb4
Aswathy Prasad <33281453+apengineer@users.noreply.github.com>
2019-04-03 07:29:19 +0200

    More solutions added

--- src/trees/problems/MediumProblems.java
+++ src/trees/problems/MediumProblems.java
@@ -1,9 +1,151 @@
 package trees.problems;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+import sun.tracing.PrintStreamProviderFactory;
 import trees.Node;
 
 //https://medium.com/@codingfreak/binary-tree-interview-questions-and-practice-problems-439df7e5ea1f
 public class MediumProblems {
+
+
+	public static void printLinkedLists(Node root) {
+		if (root == null) return;
+		int level = 0;
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		Map<Integer, LinkedList> map = new HashMap<Integer, LinkedList>();
+		while(!q.isEmpty()) {
+			Node n = q.peek();
+			level ++;
+			int count = 0;		
+			LinkedList<Node> ll = new LinkedList<Node>();
+			while(count < (2^level - 1)) {
+				Node llNode = q.peek();
+				if(llNode != null) {
+					q.add(llNode.left);
+					q.add(llNode.right);
+					ll.add(llNode);					
+				}
+				q.poll();
+				count++;
+			}
+			while(ll.iterator().hasNext()) {
+				System.out.println(ll.iterator().next());
+			}
+			map.put(level, ll);
+		}
+		
+		//printMap(map);
+	}
+
+	public static void printMap(Map map) {
+		Set<Entry<Integer, LinkedList>>myEntrySet = (Set<Entry<Integer, LinkedList>>) map.entrySet();
+		Iterator<Entry<Integer, LinkedList>> myEntrySetIt = myEntrySet.iterator();
+		
+		while(myEntrySetIt.hasNext()) {
+			Entry<Integer, LinkedList> entry = myEntrySetIt.next();
+			System.out.println("Key :" + entry.getKey() + " Value: " + entry.getValue());
+			LinkedList ll = entry.getValue();
+			while(ll.iterator().hasNext()) {
+				System.out.println(ll.iterator().next());
+			}
+		}
+		
+	}
+	public static void printNextSibling(Node root, int node) {
+		if (root == null) return;
+
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+
+		while(!q.isEmpty()) {
+			Node n = q.peek();
+			q.add(n.left);
+			q.add(n.right);
+
+			if(q.peek().data == node) {			
+				q.poll();
+				Node sibling = q.poll();
+				System.out.println("Sibling: " + sibling.data );	
+				q = null;
+				break;		
+			}
+
+			q.poll();
+
+		}
+
+	}
+
+
+	// bad implementation O(n°2) RT and O(n) space
+	public static void printSpiralOrder(Node node) {
+		int level = 0;
+		Stack<Node> s = new Stack<Node>();
+		Stack<Node> sOdd = new Stack<Node>();	
+		s.push(node);
+		while(!s.isEmpty() || !sOdd.isEmpty()) {
+			if( level % 2  == 0) { // Stack right to left
+				while(!s.isEmpty()) {
+					Node current = s.peek();
+					if(current != null) {
+						sOdd.push(current.right);
+						sOdd.push(current.left);	
+						System.out.print(current.data + " ");
+					}
+					s.pop();
+				}				
+			} 
+			else { 
+
+				while(!sOdd.isEmpty()) {
+					Node current = sOdd.peek();
+					if(current != null) {
+						s.push(current.left);
+						s.push(current.right);
+						System.out.print(current.data + " ");
+
+					}
+
+					sOdd.pop();
+				}
+
+			}	
+			level ++;
+		}	 
+
+	}
+
+
+
+	public static void printLevelOrder(Node node) {
+		if(node == null) return ;
+
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(node);
+
+		while(q.peek() != null) {
+			Node current = q.peek();
+			if(current != null)
+				System.out.print(current.data + " ");
+			if(current.left != null)
+				q.add(current.left);
+			if(current.right != null)
+				q.add(current.right);
+			q.poll();
+		}		
+
+	}
+
 
 	// delete an entire tree
 	public static Node deleteTree(Node node) {
@@ -12,7 +154,9 @@ public class MediumProblems {
 		deleteTree(node.left);
 		deleteTree(node.right);
 
-		return node = null;
+		node = null;
+
+		return node;
 
 	}
 
@@ -60,6 +204,22 @@ public class MediumProblems {
 		System.out.println(areIdentical(rootA, rootB));
 
 		System.out.println(height(rootA));
+
+		//rootA = deleteTree(rootA);
+
+		//rootA.printInOrder();
+
+		//printLevelOrder(rootA);
+
+		printSpiralOrder(rootA);
+
+		System.out.println();
+
+		printNextSibling(rootA, 5);
+		
+		System.out.println();
+		
+		printLinkedLists(rootA);
 
 
 	}
